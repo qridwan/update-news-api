2a90a42ba9e80de96d5e4e9e55ba015808de1d4f
Ridwan <myworld.shoyab@gmail.com>
2021-06-22 12:04:56 +0600

    category route edited

--- routes/categories/category_wise_news.en.js
+++ routes/categories/category_wise_news.en.js
@@ -6,7 +6,7 @@ Router.get("/en/category/:category", (req, res) => {
   const getCategory = req.params.category;
   async function getCategoryNews() {
     let allNews = [];
-    for (let i = 1; i < 10 ; i++) {
+    for (let i = 1; i < 20 ; i++) {
       const url = `https://updatenews360.herokuapp.com/en/category/${getCategory}/${i}`;
     await fetch(url)
         .then((res) => res.json())
@@ -15,7 +15,7 @@ Router.get("/en/category/:category", (req, res) => {
         })
         .catch(err => console.log(err));
     }
-    console.log(allNews.length);
+    
     // return allNews;
     res.send(allNews);
   }
